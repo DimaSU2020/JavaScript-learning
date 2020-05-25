9e97af2eb6c2fe394ae3162435a25a832f0a032d
Dima_S <dimapepsi@mail.ru>
2020-05-26 01:36:03 +0300

    Упражнение FizzBizz

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
-let grill = "#";
-let counter = 0;
-while (counter < 7) {
-    console.log(grill);
-    counter++;
-    grill = grill + "#";
+let fizz = "Fizz";
+let buzz = "Buzz";
+for (let number = 1; number <= 100; number += 1) {
+    if (number % 3 == 0 && number % 5 == 0) {
+        console.log(fizz + buzz);
+    } else if (number % 3 == 0) {
+        console.log(fizz);
+    } else if (number % 5 == 0) {
+        console.log(buzz);
+    }   else {
+            console.log(number);   
+        }
 }
