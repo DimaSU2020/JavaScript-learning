a05963adb31107f97030890c4111dac452803a16
Dima_S <dimapepsi@mail.ru>
2020-05-25 23:27:14 +0300

    Cycle for

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-for (let number = 0; number <= 1000000; number =  number + 2) {
-    console.log(number);
+let result = 1;
+for (let counter = 0; counter < 10; counter = counter + 1) {
+    result = result * 2;
 }
-
-
+console.log(result);
